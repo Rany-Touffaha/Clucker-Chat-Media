e7f523e08f86f1060808a7a3dc37a60347517121
Rany-Touffaha <79004147+Rany-Touffaha@users.noreply.github.com>
2021-10-24 23:19:05 +0100

    Continued working on the seed file
    Continued working on the seed file (not done yet!)

--- microblogs/management/commands/seed.py
+++ microblogs/management/commands/seed.py
@@ -1,13 +1,23 @@
 from django.core.management.base import BaseCommand, CommandError
 from faker import Faker
+from faker.providers import BaseProvider
 from microblogs.models import User
 
 class Command(BaseCommand):
     def __init__(self):
         super().__init__()
         self.faker = Faker('en_GB')
-        self.user = User()
 
 
     def handle(self, *args, **options):
-        print(self.faker.first_name())
+        for i in range(100):
+            try:
+                fakeUser = User.objects.create_user(username='@' + self.faker.unique.word(),
+                                                    password=self.faker.password(), first_name=self.faker.first_name(),
+                                                    last_name=self.faker.last_name(), email=self.faker.email(), bio=self.faker.text())
+                if fakeUser.full_clean():
+                    fakeUser.save()
+            except User.DoesNotExist:
+                raise CommandError('User does not exist')
+
+
