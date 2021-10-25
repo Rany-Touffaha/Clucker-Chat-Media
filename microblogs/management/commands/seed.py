ee7ca0a84070a007226f3e250a678f984cae27f4
Rany-Touffaha <79004147+Rany-Touffaha@users.noreply.github.com>
2021-10-25 11:28:52 +0100

    Added tests for post model and implemented the sign up html
    Added tests for post model and implemented the sign up html

--- microblogs/management/commands/seed.py
+++ microblogs/management/commands/seed.py
@@ -1,18 +1,17 @@
 from django.core.management.base import BaseCommand, CommandError
 from faker import Faker
-from faker.providers import BaseProvider
 from microblogs.models import User
+
 
 class Command(BaseCommand):
     def __init__(self):
         super().__init__()
         self.faker = Faker('en_GB')
 
-
     def handle(self, *args, **options):
         for i in range(100):
             try:
-                fakeUser = User.objects.create_user(username='@' + self.faker.unique.word(),
+                fakeUser = User.objects.create_user('@' + self.faker.unique.word(),
                                                     password=self.faker.password(), first_name=self.faker.first_name(),
                                                     last_name=self.faker.last_name(), email=self.faker.email(), bio=self.faker.text())
                 if fakeUser.full_clean():
