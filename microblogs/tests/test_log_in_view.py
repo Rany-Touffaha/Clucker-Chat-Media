8a63bf877ec63c72dadef9f3c3d7c86dc20b6dd6
Rany-Touffaha <79004147+Rany-Touffaha@users.noreply.github.com>
2021-10-26 22:44:08 +0100

    Update test_log_in_view.py

--- microblogs/tests/test_log_in_view.py
+++ microblogs/tests/test_log_in_view.py
@@ -17,7 +17,8 @@ class LogInViewTestCase(TestCase, LogInTester):
             last_name='Doe',
             email='johndoe@example.org',
             password='Password123',
-            bio='Hello, I am John Doe.'
+            bio='Hello, I am John Doe.',
+            is_active=True
         )
 
     def test_log_in_url(self):
@@ -48,4 +49,17 @@ class LogInViewTestCase(TestCase, LogInTester):
         response_url = reverse('feed')
         self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
         self.assertTemplateUsed(response, 'feed.html')
+
+    def test_valid_log_in_by_inactive_user(self):
+        self.user.is_active = False
+        self.user.save()
+        form_input = {'username': '@johndoe', 'password': 'Password123'}
+        response = self.client.post(self.url, form_input, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'log_in.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, LogInForm))
+        self.assertFalse(form.is_bound)
+        self.assertFalse(self._is_logged_in())
+
 
